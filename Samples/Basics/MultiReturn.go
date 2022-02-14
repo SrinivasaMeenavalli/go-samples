e00a688d832c43127c41475d37b1ce1421ce410a
srinivasmeenavalli <37342463+srinivasmeenavalli@users.noreply.github.com>
2022-02-14 14:36:33 +0530

    new changes

--- Samples/Basics/MultiReturn.go
+++ Samples/Basics/MultiReturn.go
@@ -1,7 +1,8 @@
 package main
+
 /**
 Go has built-in support for multiple return values.
- This feature is used often in idiomatic Go, 
+ This feature is used often in idiomatic Go,
  for example to return both result and error values from a function
 */
 
@@ -12,13 +13,13 @@ func main() {
 	r1, r2 := vals()
 	fmt.Println("Returned Value1,Value 2:", r1, r2)
 	/**If you only want a subset of the returned values,
-	 use the blank identifier "_"
-	 */
+	use the blank identifier "_"
+	*/
 	r3, _ := vals()
 	fmt.Println("Returned Value 1:", r3)
 
 	_, r4 := vals()
-	fmt.Println("Returned Value 2:", r
+	fmt.Println("Returned Value 2:", r4)
 
 }
 
