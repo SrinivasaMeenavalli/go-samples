b079e209bcd0b1205967220dcb60b38093794161
srinivasmeenavalli <37342463+srinivasmeenavalli@users.noreply.github.com>
2022-02-16 13:55:37 +0530

    Update Embedding.go

--- Samples/Basics/Embedding.go
+++ Samples/Basics/Embedding.go
@@ -15,12 +15,17 @@ func (b base) describe() string {
 	return fmt.Sprintf("base with num=%v", b.num)
 }
 
+/** A container embeds a base.
+An embedding looks like a field without a name
+*/
+
 type container struct {
 	base
 	str string
 }
 
 func main() {
+	//When creating structs with literals, we have to initialize the embedding explicitly
 	co := container{
 		base: base{
 			num: 1,
@@ -28,9 +33,11 @@ func main() {
 		str: "some name",
 	}
 	fmt.Printf("co={num: %v, str: %v}\n", co.num, co.str)
+	//full path using the embedded type name
 	fmt.Println("also num:", co.base.num)
 	fmt.Println("describe:", co.describe())
 
+	//Embedding structs with methods
 	type describer interface {
 		describe() string
 	}
