c89bf14f3e8a77fd1dd5dc2a8f9a00054613c219
srinivasmeenavalli <37342463+srinivasmeenavalli@users.noreply.github.com>
2022-03-11 15:46:10 +0530

    changed folder name

--- Samples/Basics/Hello.go
+++ Samples/Basics/Hello.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello Srinivas! Welcome to Go World")
+	fmt.Println("Hi")
+	fmt.Println("Hello")
+	fmt.Println("Namskara")
+	fmt.Println("Namaste")
+	fmt.Println("Namaskaram")
+	fmt.Println("Pranam")
+	fmt.Println("Vanakkam")
+	fmt.Println("Ola")
+	
+		
 }
---
+++ Samples/Concurrency/timeouts.go
@@ -0,0 +1,9 @@
+package main
+
+/*
+Timeouts are important for programs that connect
+to external resources or that otherwise need to bound execution time.
+*/
+func main() {
+
+}
