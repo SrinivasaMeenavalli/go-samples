49cbfb931b071f40ab89854a157b4cc27b7f5fca
srinivasmeenavalli <37342463+srinivasmeenavalli@users.noreply.github.com>
2022-03-25 12:26:47 +0530

    Changes

--- Samples/Errors/Panic.go
+++ Samples/Errors/Panic.go
@@ -6,12 +6,13 @@ func main() {
 	/*
 		use panic throughout this site to check for unexpected errors
 	*/
-	panic("A Problem Occured:The system cannot find the path specified")
 	/*A common use of panic is to abort if a function returns an error
 	 */
-	_, err := os.Create("/tmp/file")
+	_, err := os.Create("C:/GoLang/tmp/newfile/file.txt")
 	if err != nil {
 		panic(err)
+		//panic("A Problem Occured:The system cannot find the path specified")
+
 	}
 	/*
 		When first panic in main fires, the program exits without reaching the rest of the code
