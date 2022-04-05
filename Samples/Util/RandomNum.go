71a6cb2c91b053db2898a1fc12c7e855ed870e9c
srinivasmeenavalli <37342463+srinivasmeenavalli@users.noreply.github.com>
2022-04-05 16:42:06 +0530

    Number Parsing, Random Numbers

--- Samples/Util/RandomNum.go
+++ Samples/Util/RandomNum.go
@@ -7,9 +7,14 @@ import (
 )
 
 func main() {
+	/*
+		rand.Intn returns a random int n, 0 <= n < 100.
+	*/
 	fmt.Print(rand.Intn(100), ",")
 	fmt.Print(rand.Intn(100))
 	fmt.Println()
+	/*rand.Float64 returns a float64 f, 0.0 <= f < 1.0.
+	 */
 	fmt.Println(rand.Float64())
 	fmt.Print((rand.Float64()*5)+5, ",")
 	fmt.Print((rand.Float64() * 5) + 5)
@@ -21,6 +26,10 @@ func main() {
 	fmt.Print(r1.Intn(100))
 	fmt.Println()
 
+	/*
+		If you seed a source with the same number,
+		it produces the same sequence of random numbers
+	*/
 	s2 := rand.NewSource(42)
 	r2 := rand.New(s2)
 	fmt.Print(r2.Intn(100), ",")
