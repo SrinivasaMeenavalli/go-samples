8fa86c423e91f1a97bcb72e29417ad67334838c4
srinivasmeenavalli <37342463+srinivasmeenavalli@users.noreply.github.com>
2022-04-07 18:41:18 +0530

    changes

--- Samples/Util/Base64.go
+++ Samples/Util/Base64.go
@@ -9,13 +9,21 @@ import (
 )
 
 func main() {
+	//Go supports both standard and URL-compatible base64
+
 	data := "abc123!?$*&()'-=@~"
 	sEnc := b64.StdEncoding.EncodeToString([]byte(data))
 	fmt.Println(sEnc)
+	/*
+		Decoding may return an error, which you can check
+		if you don’t already know the input to be well-formed
+	*/
 	sDec, _ := b64.StdEncoding.DecodeString(sEnc)
 	fmt.Println(string(sDec))
 	fmt.Println()
-
+	/*
+		This encodes/decodes using a URL-compatible base64 format
+	*/
 	uEnc := b64.URLEncoding.EncodeToString([]byte(data))
 	fmt.Println(uEnc)
 	uDec, _ := b64.URLEncoding.DecodeString(uEnc)
